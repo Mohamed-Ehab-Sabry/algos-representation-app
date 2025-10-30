db46801737b877c6daf640b688dcbf4e7e670c69
Abdo-Islam <abdoislam2050@gmail.com>
2025-10-30 17:12:44 +0300

    added a counter to each Search method and added a menu to the main function

--- Search.cpp
+++ Search.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
 using namespace std;
 
+
+int seqCounter; 
+
 int seqSearch(int arr[], int n, int x)
 {
     for (int i = 0; i <= n ; i++)
     {
+        seqCounter++; 
         if(arr[i] == x)
             return i;
     }
     return -1;
 }
 
+
+int rSeqCounter; 
+
 int recSeqSearch(int arr[], int n, int x)
 {
+    rSeqCounter++; 
     if (n == -1)
         return -1;
     if(arr[n] == x)
@@ -20,11 +28,17 @@ int recSeqSearch(int arr[], int n, int x)
     return recSeqSearch(arr, n-1, x);
 }
 
+
+
+
+int binaryCounter; 
+
 int binarySearch(int arr[], int n, int x)
 {
     int l = 0, r = n;
     while(l <= r)
     {
+        binaryCounter++; 
         int mid = l + (r - l) /2;
         if(arr[mid] == x)
             return mid;
@@ -36,11 +50,17 @@ int binarySearch(int arr[], int n, int x)
     return -1;
 }
 
+
+
+
+int rBinaryCounter; 
+
 int recBinarySearch(int arr[], int l, int r, int x)
 {
     if(l > r)
         return -1;
     int mid = l + (r - l) /2;
+    rBinaryCounter++; 
     if(arr[mid] == x)
         return mid;
     else if (arr[mid] < x)
@@ -49,9 +69,88 @@ int recBinarySearch(int arr[], int l, int r, int x)
         return recBinarySearch(arr, l, mid-1, x);
 }
 
+
+
+
 int main()
 {
-    
+    const int SIZE = 50;
+    int arr[SIZE];
+    for (int i = 0; i < SIZE; ++i)
+        arr[i] = i + 1; // sorted array 1..50 for binary searches
 
+    int choice = 0;
+    while (true)
+    {
+        cout << "\nArray:\n";
+        for (int i = 0; i < SIZE; ++i)
+        {
+            cout << arr[i] << (i % 10 == 9 ? '\n' : ' ');
+        }
+
+        cout << "\nChoose search method:\n"
+             << "1. Sequential Search (iterative)\n"
+             << "2. Sequential Search (recursive)\n"
+             << "3. Binary Search (iterative)\n"
+             << "4. Binary Search (recursive)\n"
+             << "5. Exit\n"
+             << "Enter choice: ";
+        if (!(cin >> choice))
+        {
+            cin.clear();
+            cin.ignore(10000, '\n');
+            cout << "Invalid input. Try again.\n";
+            continue;
+        }
+        if (choice == 5)
+            break;
+        if (choice < 1 || choice > 5)
+        {
+            cout << "Invalid choice. Try again.\n";
+            continue;
+        }
+
+        int x;
+        cout << "Enter number to search: ";
+        if (!(cin >> x))
+        {
+            cin.clear();
+            cin.ignore(10000, '\n');
+            cout << "Invalid input. Try again.\n";
+            continue;
+        }
+
+        int result = -1;
+        switch (choice)
+        {
+            case 1:
+                seqCounter = 0;
+                result = seqSearch(arr, SIZE - 1, x);
+                cout << "seqSearch entered: " << seqCounter << " times\n";
+                break;
+            case 2:
+                rSeqCounter = 0;
+                result = recSeqSearch(arr, SIZE - 1, x);
+                cout << "recSeqSearch entered: " << rSeqCounter << " times\n";
+                break;
+            case 3:
+                binaryCounter = 0;
+                result = binarySearch(arr, SIZE - 1, x);
+                cout << "binarySearch entered: " << binaryCounter << " times\n";
+                break;
+            case 4:
+                rBinaryCounter = 0;
+                result = recBinarySearch(arr, 0, SIZE - 1, x);
+                cout << "recBinarySearch entered: " << rBinaryCounter << " times\n";
+                break;
+        }
+
+        if (result != -1)
+            cout << "Number found at index: " << result << '\n';
+        else
+            cout << "Number not found\n";
+    }
+
+    cout << "Exiting.\n";
     return 0;
 }
